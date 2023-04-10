32bc2cde445da77ce1a837ba966d62718c1688c7
Matthews Brito <109427217+Matthewsfx05@users.noreply.github.com>
2023-04-10 12:19:58 -0300

    Add files via upload

---
+++ ARRAYS/script.js
@@ -0,0 +1,27 @@
+// let array = ["Poha", "Brito", 17, true]
+// Não é uma boa prática
+
+// let people = ['Leticia', 'Erick', 'Pedro', 'Paula'];
+// Boa prática
+// console.log(arrayValid[0]);
+
+// for(let person of people) {
+    // console.log(person);
+// }   
+
+// people.push('Mago')
+
+// console.log(people)
+
+const people = [
+    [
+        'Fernando',
+        30,
+        'RJ'
+    ],
+    [
+        'Gabriela',
+        28,
+        'SP'
+    ]
+]
